2761e457212e836afee82a2ee3d170444f078b93
iiamwendy <widadtaouaf03@gmail.com>
2023-11-06 14:33:13 +0100

    modifica file router(path e element)

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -2,21 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import PageRoot from "../pages/pageRoot";
 import App from "../App";
 import PageError from "../pages/pageError";
-import PageReverberi from "../pages/pageReverberi";
+import PageTaouaf from "../pages/pageTaouaf";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <PageRoot />,
     errorElement: <PageError />,
+
+    
     children: [
       {
         index: true,
         element: <App />,
       },
       {
-        path: "reverberi",
-        element: <PageReverberi />,
+        path: "taouaf",
+        element: <PageTaouaf />,
       },
     ],
   },
